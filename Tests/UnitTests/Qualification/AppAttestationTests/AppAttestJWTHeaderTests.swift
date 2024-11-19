f527906bd2139f2d1034e285f4151e96e0066d37
magdaline <magdaline.amoakohene@digital.cabinet-office.gov.uk>
2024-11-19 12:30:18 +0000

    test: adding tests back in

---
+++ Tests/UnitTests/Qualification/AppAttestationTests/AppAttestJWTHeaderTests.swift
@@ -0,0 +1,11 @@
+@testable import OneLogin
+import Testing
+
+struct AppAttestJWTHeaderTests {
+    @Test("Initialise header from value")
+    func initialiseJWTHeader() {
+        let header = AppAttestJWTHeader(alg: "xyz")
+        
+        #expect(header.value == ["alg": "xyz"])
+    }
+}
---
+++ Tests/UnitTests/Qualification/AppAttestationTests/AppAttestJWTPayloadTests.swift
@@ -0,0 +1,15 @@
+@testable import OneLogin
+import Testing
+
+struct AppAttestJWTPayloadTests {
+    @Test("Initialise payload from value")
+    func initialiseJWTPayload() {
+        let payload = AppAttestJWTPayload(issuer: "rty",
+                                 audience: "dfghj",
+                                 jwtID: "bnj")
+        
+        #expect(payload.value == ["iss": "rty",
+                                  "aud": "dfghj",
+                                  "jti": "bnj"])
+    }
+}
